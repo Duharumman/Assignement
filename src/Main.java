d0c4cfabfe616f7927b27c574b5fd900697c65ae
Duha <duha.abu99@gmail.com>
2021-01-08 02:56:39 +0200

    apply

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Jordan (:");
+
+        System.out.println("We are working in a new assignement xD");
     }
 }
